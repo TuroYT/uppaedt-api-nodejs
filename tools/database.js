2e154fe52372bf8e81e495bbd64fcd88e299b720
Romain PINSOLLE <romain.pins@gmail.com>
2024-10-24 17:22:59 +0200

    syncPlannings() finished

--- tools/database.js
+++ tools/database.js
@@ -1,10 +1,24 @@
 /*
-    * Database connection
+
+? Auteur : Romain PINSOLLE
+? fichier qui gère la connection à la base de donnée
+
+
+! Listes des fontions de cette page  
+
+* insertCourse(idGroupe, nomCours, dateDeb, dateFin, prof, lieu)
+* getAllFormation()
+* addGroup(idGroupe, nomGroupe, idFormation)
+* getAllGroups()
+* getGroupsFromFormation(formationId)
+* getIcsLinks(formationId)
+? syncPlannings() sync des plannings 
+
 */
 
-const mysql = require("mysql")
+const mysql = require("mysql");
 const { getIcalFromWeb, prepareIcalForDB } = require("./gestionPlanning");
-require('dotenv').config();
+require("dotenv").config();
 
 // Récupération des variables d'environnement
 const DB_HOST = process.env.DB_HOST;
@@ -14,37 +28,42 @@ const DB_NAME = process.env.DB_NAME;
 
 // Vérification des variables d'environnement
 if (!DB_HOST || !DB_USER || !DB_PASS || !DB_NAME) {
-    console.error('Missing environment variables. Please check the .env file.');
-    process.exit(1);
+  console.error("Missing environment variables. Please check the .env file.");
+  process.exit(1);
 }
 
-// Création de la connexion à la base de données
+//  ! Création de la connexion à la base de données
 const pool = mysql.createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
 });
 
 
-async function insertCourse(idGroupe, nomCours, dateDeb, dateFin, prof, lieu) {
-    const QUERY = `
-        INSERT INTO uppaCours (idGroupe, nomCours, dateDeb, dateFin, prof, lieu)
-        VALUES (?, ?, ?, ?, ?, ?, ?);
-    `;
-    
-    return new Promise((resolve, reject) => {
-        pool.query(QUERY, [idGroupe, nomCours, dateDeb, dateFin, prof, lieu], (err, results) => {
-            if (err) {
-                console.error('Error inserting course:', err);
-                reject(err);
-                return;
-            }
-            resolve(results);
-        });
-    });
-}
 
+
+async function insertCourse(idGroupe, nomCours, dateDeb, dateFin, prof, lieu) {
+  const QUERY = `
+        INSERT INTO \`uppaCours\` (\`idGroupe\`, \`nomCours\`, \`dateDeb\`, \`dateFin\`, \`prof\`, \`lieu\`)
+        VALUES (?, ?, ?, ?, ?, ?);
+    `;
+
+  return new Promise((resolve, reject) => {
+    pool.query(
+      QUERY,
+      [idGroupe, nomCours, dateDeb, dateFin, prof, lieu],
+      (err, results) => {
+        if (err) {
+          console.error("Error inserting course:", err);
+          reject(err);
+          return;
+        }
+        resolve(results);
+      }
+    );
+  });
+}
 
 /**
  * Retrieves all formations from the database.
@@ -63,149 +82,217 @@ async function insertCourse(idGroupe, nomCours, dateDeb, dateFin, prof, lieu) {
  * 
  */
 const getAllFormation = async () => {
-    // * Récupération de toutes les formations
-    const QUERY = "SELECT * FROM `uppaFormation` ORDER BY nom;";
-    
-    return new Promise((resolve, reject) => {
-        pool.query(QUERY, (err, results) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-                return;
-            }
-            resolve(results);
-        });
+  // * Récupération de toutes les formations
+  const QUERY = "SELECT * FROM 'uppaFormation' ORDER BY nom;";
+
+  return new Promise((resolve, reject) => {
+    pool.query(QUERY, (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      resolve(results);
     });
+  });
 };
 
+/**
+ * Adds a new group to the database.
+ *
+ * @param {number} idGroupe - The ID of the group.
+ * @param {string} nomGroupe - The name of the group.
+ * @param {number} idFormation - The ID of the formation.
+ * @returns {Promise<number>} A promise that resolves to the ID of the created group.
+ */
+const addGroup = (idGroupe, nomGroupe, idFormation) => {
+  const QUERY =
+    "INSERT INTO 'uppaGroupe' (idGroupe, nomGroupe, idFormation) VALUES (?, ?, ?);";
 
-const getAllGroups = () => {
-    const QUERY = "SELECT * FROM `uppaGroupe` ORDER BY nom;";
-    
-    return new Promise((resolve, reject) => {
-        pool.query(QUERY, (err, results) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-                return;
-            }
-            resolve(results);
-        });
+  return new Promise((resolve, reject) => {
+    pool.query(QUERY, [idGroupe, nomGroupe, idFormation], (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      // Return the ID of the created group
+      resolve(results.insertId);
     });
-}
+  });
+};
 
+/**
+ * Retrieves all groups from the 'uppaGroupe' table, ordered by the 'nom' column.
+ *
+ * @returns {Promise<Object[]>} A promise that resolves to an array of group objects.
+ * @throws {Error} If there is an error executing the query.
+ */
+const getAllGroups = () => {
+  const QUERY = "SELECT * FROM 'uppaGroupe' ORDER BY nom;";
+
+  return new Promise((resolve, reject) => {
+    pool.query(QUERY, (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
+};
 
 const getGroupsFromFormation = (formationId) => {
-    const QUERY = "SELECT * from `uppaGroupe` WHERE `idFormation` = ?";
+  const QUERY = "SELECT * from 'uppaGroupe' WHERE 'idFormation' = ?";
 
-    return new Promise((resolve, reject) => {
-        pool.query(QUERY, [formationId], (err, results) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-                return;
-            }
-            resolve(results);
-        });
+  return new Promise((resolve, reject) => {
+    pool.query(QUERY, [formationId], (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      resolve(results);
     });
-}
+  });
+};
 
 /**
  * Retrieves the ICS link for a given group ID from the database.
  *
- * @param {Numbereee} groupId - The ID of the group for which to retrieve the ICS link.
+ * @param {formationId} formationId - The ID of the formation for which to retrieve the ICS link.
  * @returns {Promise<String>} A promise that resolves to the ICS link of the specified group.
  */
 const getIcsLinks = (formationId) => {
-    const QUERY = "SELECT `lienICS` FROM `icsLink` WHERE idFormation = ? ;";
-    
-    return new Promise((resolve, reject) => {
-        pool.query(QUERY, [formationId], (err, results) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-                return;
-            }
-            let toReturn = []
-            results.map((ics) => {toReturn.push(ics.lienICS)})
-            resolve(toReturn);
-        });
+  const QUERY = "SELECT 'lienICS' FROM 'icsLink' WHERE idFormation = ? ;";
+
+  return new Promise((resolve, reject) => {
+    pool.query(QUERY, [formationId], (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      let toReturn = [];
+      results.map((ics) => {
+        toReturn.push(ics.lienICS);
+      });
+      resolve(toReturn);
     });
-}
+  });
+};
 
+/**
+ * Retrieves all active ICS links from the database.
+ *
+ * This function executes a SQL query to select `lienICS` and `idFormation` 
+ * from the `icsLink` table, joining with the `uppaFormation` table to ensure 
+ * that only active formations (`estActif` = 1) are included.
+ *
+ * @returns {Promise<Object[]>} A promise that resolves to an array of objects, 
+ * each containing `lienICS` and `idFormation` properties.
+ *
+ * @throws {Error} If there is an error executing the query, the promise is rejected with the error.
+ */
+const getAllIcsLinks = () => {
+  const QUERY = `
+        SELECT \`lienICS\`, \`uppaFormation\`.\`idFormation\`
+        FROM \`icsLink\`
+        JOIN \`uppaFormation\` ON \`icsLink\`.\`idFormation\` = \`uppaFormation\`.\`idFormation\`
+        WHERE \`uppaFormation\`.\`estActif\` = 1;
+    `;
 
-// ! A continuer a partir d'ici
+  return new Promise((resolve, reject) => {
+    pool.query(QUERY, (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+
+      resolve(results);
+    });
+  });
+};
+
+/**
+ * Synchronizes planning data by performing the following steps:
+ * 1. Deletes future courses from the `uppaCours` table.
+ * 2. Retrieves all ICS links.
+ * 3. Fetches and processes iCal data from each ICS link.
+ * 4. Inserts new courses into the database if they are scheduled for the future.
+ * 
+ * @async
+ * @function syncPlannings
+ * @returns {Promise<Object>} An object containing an error flag and the number of synchronized courses.
+ * @property {boolean} error - Indicates if there was an error during synchronization.
+ * @property {number} nbSynced - The total number of courses synchronized.
+ */
 const syncPlannings = async () => {
+    let thereIsAnError = false;
+    global.totalSynced = 0;
+  // supprimer les cours futurs
+  const QUERY = `
+        DELETE FROM \`uppaCours\` WHERE \`dateDeb\` > NOW();
+    `;
+  pool.query(QUERY, async (err, results) => {
+    if (err) {
+      console.error("Error deleting future courses:", err);
+      return;
+    }
+    console.log("Deleted future courses:", results.affectedRows);
+  });
+  // * get all ics links
+  let icsLinks = await getAllIcsLinks();
+  let listGroup = [];
 
-    // supprimer les cours futurs
-    const QUERY = "DELETE FROM `planning` WHERE dateDeb > NOW();";
-    pool.query(QUERY, async (err, results) => {
-        if (err) {
-            console.error('Error deleting future courses:', err);
-            return;
+  const doSync = async () => { for (let i in icsLinks) {
+    await getIcalFromWeb(icsLinks[i].lienICS)
+      .then((ical) => {
+        let parsedIcal = prepareIcalForDB(ical);
+
+        for (k in parsedIcal) {
+          currentCourse = parsedIcal[k];
+          const groupId = icsLinks[i].idFormation + "-" + currentCourse.nomTp;
+
+          if (currentCourse.dateDeb > new Date()) {
+            // idGroupe, nomCours, dateDeb, dateFin, prof, lieu
+            insertCourse(
+              groupId,
+              currentCourse.nomCours,
+              currentCourse.dateDeb,
+              currentCourse.dateFin,
+              currentCourse.prof,
+              currentCourse.lieu,
+              currentCourse.nomTp
+            );
+          }
         }
-        console.log('Deleted future courses:', results.affectedRows);
-    
-    
-    
-    
-        const groups = await getAllGroups()
-    
 
-        for (let i in groups){
-    
-            // verif si le group est actif
-            if (groups[i].active === 0) {
-                continue;
-            }
-    
-            getIcalFromWeb(groups[i].lien_ics)
-            .then((ical) =>{
-                let parsedIcal = prepareIcalForDB(ical, groups[i].id)
-                
-                for (k in parsedIcal) {
-                    currentCourse = parsedIcal[k]
-                    if (currentCourse.dateDeb > new Date)
-                    {
-                        insertCourse(currentCourse.groupId, currentCourse.nomCours, currentCourse.dateDeb, currentCourse.dateFin, currentCourse.prof, currentCourse.lieu, currentCourse.nomTp)
-                    }
-    
-                }
-    
-    
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    
-    
-    
-    
-        }
-    
-    
-        
-    
-        console.log("Base de donnée syncronisé")
-    
-    
-    
-    
-    
-    
-    
-    });
+        global.totalSynced += parsedIcal.length;
+        console.log(parsedIcal.length,  " courses synced")
 
-    
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      
+      
+  }}
 
+  await doSync().catch((err) => {
+    thereIsAnError = true;
+    console.log(err)
+  })
+  
 
-}
+  return({error: thereIsAnError, nbSynced : global.totalSynced});
+};
 
-
-syncPlannings()
 
 
 exports.getAllFormation = getAllFormation;
 exports.getAllGroups = getAllGroups;
 exports.getGroupsFromFormation = getGroupsFromFormation;
-exports.getIcsLink = getIcsLink;
+//exports.getIcsLink = getIcsLink;
